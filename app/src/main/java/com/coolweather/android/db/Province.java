65244854ac861bc4681d829891402f7dd940acf4
JinyuChenRM <Jinyu.Chen@routematch.com>
2020-03-19 15:09:53 +0000

    new

--- app/src/main/java/com/coolweather/android/db/Province.java
+++ app/src/main/java/com/coolweather/android/db/Province.java
@@ -18,4 +18,19 @@ public class Province extends DataSupport {
         this.id = id;
     }
 
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
 }
